3eacc19b3fd316ea366f25a0bfb7b9efa0b009b6
vpaninopol-dn <vpaninopol.drivenets.com>
2023-02-06 12:00:02 +0200

    First commit in Git

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print("Hello World.", "This is a new line", sep='!!!')
+print("This is a new line.")
+print("Hello World.", "This is a new line", sep='!!!')
+print("Hello World.", "This is a new line", sep='!!!')
+
+
